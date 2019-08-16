870116b61723e4563e1e56190e40c2dd8fba6183
raysilent7 <rgc471@hotmail.com>
2019-08-16 16:28:59 -0300

    DTO class and instantiation config

--- src/main/java/com/raysilent/workshopspringboot/resources/UserResource.java
+++ src/main/java/com/raysilent/workshopspringboot/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.raysilent.workshopspringboot.resources;
 
 import com.raysilent.workshopspringboot.domain.User;
+import com.raysilent.workshopspringboot.dto.UserDto;
 import com.raysilent.workshopspringboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
