17c581438ee1c0117427148f787e1ca3ba15c25b
raysilent7 <rgc471@hotmail.com>
2019-08-19 16:54:09 -0300

    findByAny (title, body and comments)

--- src/main/java/com/raysilent/workshopspringboot/repository/PostRepository.java
+++ src/main/java/com/raysilent/workshopspringboot/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -12,5 +13,12 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{'title': { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2} } ," +
+            " { $or: [ {'title': { $regex: ?0, $options: 'i' } }, " +
+            "{'body': { $regex: ?0, $options: 'i' } }, " +
+            "{'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> findByAny(String text, Date minDate, Date maxDate);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
