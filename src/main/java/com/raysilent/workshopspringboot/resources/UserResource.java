27227e7769f7ecccb49d9aba1e15203c77038c8a
raysilent7 <rgc471@hotmail.com>
2019-08-19 15:13:13 -0300

    postsById

--- src/main/java/com/raysilent/workshopspringboot/resources/UserResource.java
+++ src/main/java/com/raysilent/workshopspringboot/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.raysilent.workshopspringboot.resources;
 
+import com.raysilent.workshopspringboot.domain.Post;
 import com.raysilent.workshopspringboot.domain.User;
 import com.raysilent.workshopspringboot.dto.UserDto;
 import com.raysilent.workshopspringboot.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
---
+++ src/main/java/com/raysilent/workshopspringboot/services/PostService.java
@@ -0,0 +1,21 @@
+package com.raysilent.workshopspringboot.services;
+
+import com.raysilent.workshopspringboot.domain.Post;
+import com.raysilent.workshopspringboot.repository.PostRepository;
+import com.raysilent.workshopspringboot.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
